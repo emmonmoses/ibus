1050ceb26c8a7a77bf388650d023858f256f9903
Appliedline Technology <appliedline.tech@gmail.com>
2024-05-29 14:33:34 +0300

    paginationParams Updated

--- API/utilities/pagination_utility.js
+++ API/utilities/pagination_utility.js
@@ -1,11 +1,13 @@
+require("dotenv").config();
 const Pagination = require("../models/pagination");
 
 module.exports = {
   paginationParams: async (req, totalItems) => {
-    const defaultPageSize = 10
+    const defaultPage = process.env.DEFAULT_PAGE;
+    const defaultPageSize = process.env.DEFAULT_PAGESIZE;
     const pagination = new Pagination({});
 
-    pagination.page = parseInt(req.query.page) || 1;
+    pagination.page = parseInt(req.query.page) || defaultPage;
     pagination.pageSize = parseInt(req.query.pageSize) || defaultPageSize;
     pagination.rows = totalItems;
     pagination.pages = Math.ceil(totalItems / pagination.pageSize);
